10e003a0fb4f00395a632f4959a93e3c5a18d276
ndlam-tma <ndlam@tma.com.vn>
2025-08-15 16:12:30 +0700

    d

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { VerifySuccessComponent } from './verify-success/verify-success.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'verify-success', component: VerifySuccessComponent },
+  { path: '', component: LandingPageComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/verify-success/verify-success.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-verify-success',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './verify-success.component.html',
+  styleUrl: './verify-success.component.scss',
+})
+export class VerifySuccessComponent {}
